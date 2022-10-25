7b918304a8f1fb644fc24123fb541a2464eef044
Andy Yudkin <andy.yudkin@gmail.com>
2022-10-25 18:18:12 +0200

    refine

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {userLogin} from '../API/userLogin';
-// import { Box } from '@mui/system';
 import { TextField, Button, Typography, Box } from '@mui/material';
 
-class Login extends React.Component {
+import { userLogin } from '../../api/userLogin';
+
+export class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,58 +12,83 @@ class Login extends React.Component {
       userId: '',
       error: ''
     };
-  } 
-  
-  userLoginCall = async () => {
-    let userData = await userLogin( this.state.username, this.state.password);
-    this.setState({userId: userData.id, error: userData.error, username: '', password: ''});
   }
-  
-  getData = (event , field) => this.setState(() => ({[field]: event.target.value}));
 
-  render() { 
-    return ( 
+  userLoginCall = async () => {
+    let userData = await userLogin(this.state.username, this.state.password);
+    this.setState({
+      userId: userData.id,
+      error: userData.error,
+      username: '',
+      password: ''
+    });
+  };
+
+  getData = (event, field) =>
+    this.setState(() => ({ [field]: event.target.value }));
+
+  render() {
+    return (
       <>
-      <Typography variant="h5" sx={{textAlign: 'center', mt: 8}}>
-        Login
-      </Typography>
-      <Box component="div" id='form' sx={{ pt: 5, border: '1px solid black', borderRadius: '10px', width: 350, height: 200, mx: 'auto', mt: 2, display: 'block', textAlign: 'center', backgroundColor: '#123456'}}> 
-        <TextField 
-          id="email" 
-          label="Email"
-          variant="outlined" 
-          value={this.state.username}
-          onChange={event => this.getData(event, 'username')}
-          size="small"
-          margin="dense"
+        <Typography variant="h5" sx={{ textAlign: 'center', mt: 8 }}>
+          Login
+        </Typography>
+        <Box
+          component="div"
+          id="form"
+          sx={{
+            pt: 5,
+            border: '1px solid black',
+            borderRadius: '10px',
+            width: 350,
+            height: 200,
+            mx: 'auto',
+            mt: 2,
+            display: 'block',
+            textAlign: 'center'
+          }}
+        >
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            value={this.state.username}
+            onChange={(event) => this.getData(event, 'username')}
+            size="small"
+            margin="dense"
           />
-          <TextField 
-          id="pass" 
-          label="Password"
-          variant="outlined" 
-          value={this.state.password}
-          onChange={event => this.getData(event, 'password')}
-          size="small"
-          margin="dense"
+          <TextField
+            id="pass"
+            label="Password"
+            variant="outlined"
+            value={this.state.password}
+            onChange={(event) => this.getData(event, 'password')}
+            size="small"
+            margin="dense"
           />
-          <Button variant="contained" onClick={this.userLoginCall} sx={{display: 'block', mt: 2, mx: 'auto'}}>Войти</Button>
-          
-      </Box>
-      { this.state.userId !== '' &&
-        <Typography  variant="h5" sx={{textAlign: 'center', mt: 2}}>
-          Идентификатор пользователя: {this.state.userId}
-        </Typography> 
-        }
-      { this.state.error !== '' &&
-        <Typography id='error' variant="h5" sx={{textAlign: 'center', mt: 2}}>
-          {this.state.error}
-        </Typography> 
-      }
-      
+          <Button
+            variant="contained"
+            onClick={this.userLoginCall}
+            sx={{ display: 'block', mt: 2, mx: 'auto' }}
+          >
+            Войти
+          </Button>
+        </Box>
+        {this.state.userId !== '' && (
+          <Typography variant="h5" sx={{ textAlign: 'center', mt: 2 }}>
+            Идентификатор пользователя: {this.state.userId}
+          </Typography>
+        )}
+        {this.state.error !== '' && (
+          <Typography
+            id="error"
+            variant="h5"
+            sx={{ textAlign: 'center', mt: 2 }}
+          >
+            {this.state.error}
+          </Typography>
+        )}
       </>
-      
-     
     );
   }
 }
-export default Login;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Login from './components/Login/Login';
+
+import { Login } from './components/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
